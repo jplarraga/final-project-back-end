b8ec776388cf59733d2020ee2ab59543a8016f25
jplarraga <jplarraga@gmail.com>
2022-02-14 00:19:20 -0600

    cors url change

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const router = require('./routes/routes')
 
 app.use(express.json())
 app.use(cors({
-    origin: 'http://localhost:3000'
+    origin: 'https://final-project-front-bank-app.herokuapp.com/'
 }))
 
 //Main Route
