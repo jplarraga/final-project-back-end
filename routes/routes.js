2783b6a4ab5ac246ae959dbba18d12eb01fdc7cf
jplarraga <jplarraga@gmail.com>
2022-02-14 00:02:31 -0600

    route fix

--- routes/routes.js
+++ routes/routes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const {welcome, getAllData, createData, getOneItem, updateData, deleteData} = require('../controllers/CrudController')
 
 
-router.route('/').get(welcome)
+//router.route('/').get(welcome)
 router.route('/users/').get(getAllData).post(createData)
 router.route('/:itemID').get(getOneItem).patch(updateData).delete(deleteData);
 
